d31c1909b3e53fba97a2a32d7f05d62c075f5729
koyi <2573354879@qq.com>
2022-07-22 10:21:27 +0800

    first commit

--- src/main/java/com/example/test/gitTest.java
+++ src/main/java/com/example/test/gitTest.java
@@ -3,6 +3,7 @@ package com.example.test;
 public class gitTest {
     public static void main(String[] args){
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 
 }
